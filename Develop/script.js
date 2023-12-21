0ce61fe1021c67ad34ec4df45363770d1ae27904
kal <kalleepar@gmail.com>
2023-12-21 18:59:38 -0500

    fixed localstorage bug

--- Develop/script.js
+++ Develop/script.js
@@ -45,16 +45,16 @@ $(function () {
     }
 
     // local items
-    $("#hour8 .description").val(localStorage.getItem("hour9"));
-    $("#hour9 .description").val(localStorage.getItem("hour10"));
-    $("#hour10 .description").val(localStorage.getItem("hour11"));
-    $("#hour11 .description").val(localStorage.getItem("hour12"));
-    $("#hour12 .description").val(localStorage.getItem("hour13"));
-    $("#hour13 .description").val(localStorage.getItem("hour14"));
-    $("#hour14 .description").val(localStorage.getItem("hour15"));
-    $("#hour15 .description").val(localStorage.getItem("hour16"));
-    $("#hour16 .description").val(localStorage.getItem("hour17"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
     $("#hour17 .description").val(localStorage.getItem("hour17"));
+   
 
     timeTracker();
 })
